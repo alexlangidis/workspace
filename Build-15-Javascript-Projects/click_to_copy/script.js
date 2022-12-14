62acaa1ce7f5b6c448fb1738366efab81cb55926
alexlag7 <alexlag.dev@gmail.com>
2022-12-14 19:16:54 +0200

    main

--- Build-15-Javascript-Projects/click_to_copy/script.js
+++ Build-15-Javascript-Projects/click_to_copy/script.js
@@ -6,8 +6,8 @@ btn.addEventListener("click", copyText);
 function copyText(e) {
     e.preventDefault();
     coupon.select();
-    document.execCommand("copy");
-
+    const text = coupon.value;
+    navigator.clipboard.writeText(text);     // document.execCommand("copy");
 
     btn.innerHTML = `<i class="fas fa-copy"></i> Copied`;
     setTimeout(()=> {
