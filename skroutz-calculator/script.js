ad2b1be6fa058c8c5474c789227fd22aae91138f
alexlag7 <alexlag.dev@gmail.com>
2022-12-19 19:43:30 +0200

    main

---
+++ skroutz-calculator/script.js
@@ -0,0 +1,61 @@
+const agora = document.querySelector(".agora");
+const lianiki = document.querySelector(".lianiki");
+const pososto = document.querySelector(".pososto");
+const reset = document.querySelector(".reset");
+const btn = document.querySelector(".btn");
+const profit1 = document.querySelector(".profit1");
+const error = document.querySelector(".error");
+const input = document.querySelector("input");
+
+
+function caclProfit (e) {
+    e.preventDefault();
+    const lianikiValue = lianiki.value;
+    const posostoValue = pososto.value;
+    const agoraValue = agora.value;
+
+    const ypol1 = Number(lianikiValue) * Number(-posostoValue);
+    const ypol2 = Number(lianikiValue) + Number(ypol1);
+    const ypol3 = Number(ypol2 / 1.24);
+
+    const profit = Number(ypol3) - Number(agoraValue);
+    const roundProfit = profit.toFixed(2);
+    profit1.innerHTML= `${roundProfit}`;
+
+
+
+
+    if (lianikiValue === "" || agoraValue === "") {
+        error.style.display = "block";
+        error.innerHTML = `Βαλτε Τιμ. Αγορας & Λιανικης`;
+    } else if (isNaN((lianikiValue) || isNaN(agoraValue))){
+        error.style.display = "block";
+        error.innerHTML = `Παρακαλω βαλτε αριθμο`;
+    }  else if (roundProfit <= 0.7){
+        error.style.display = "block";
+        profit1.style.color = "red";
+        error.innerHTML = `Low Profit`;
+    } else {
+        profit1.style.color = "white";
+        error.style.display = "none";
+    }
+
+}
+
+
+
+btn.addEventListener("click", caclProfit);
+
+document.body.addEventListener('keypress', (e) =>{
+    if(e.key === "Enter") {
+      caclProfit(e);
+    }
+  });
+
+
+reset.addEventListener("click", ()=>{
+    document.querySelector(".form1").reset();
+    profit1.innerHTML= `0`;
+    profit1.style.color = "white";
+    error.style.display = "none";
+});
