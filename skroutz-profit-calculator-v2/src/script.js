51d86865e725b224dcd03a708ffbeaaeb6ff9fcb
alexlag7 <alexlag.dev@gmail.com>
2024-01-30 16:00:52 +0200

    main

---
+++ skroutz-profit-calculator-v2/src/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  function caclProfit(e) {
+    e.preventDefault();
+
+    const agoraValue = $("#agora").val().replace(/,/g, ".");
+    const lianikiValue = $("#lianiki").val().replace(/,/g, ".");
+    const posostoValue = $("#pososto").val();
+    const fulfilledValue = $("#fulfilled").val();
+
+    const ypol1 = Number(lianikiValue) * Number(-posostoValue);
+    const ypol2 = Number(lianikiValue) + Number(ypol1);
+    const ypol3 = Number(ypol2 / 1.24);
+
+    const profit = Number(ypol3) - Number(agoraValue) - Number(fulfilledValue);
+    const roundProfit = profit.toFixed(2);
+    $("#profit").html(`${roundProfit}`);
+
+    if (lianikiValue === "" || agoraValue === "") {
+      $(".error").css("display", "flex");
+      $(".error").html(`Βαλτε Τιμ. Αγορας & Τιμ. Λιανικης`);
+    } else if (roundProfit < 0) {
+      $(".error").css("display", "block");
+      $("#profit").css("color", "red");
+      $(".error").html(`No Profit`);
+    } else if (roundProfit <= 0.7) {
+      $(".error").css("display", "block");
+      $("#profit").css("color", "red");
+      $(".error").html(`Low Profit`);
+    } else {
+      $("#profit").css("color", "#b2ff00");
+      $(".error").css("display", "none");
+    }
+  }
+
+  $("#btn_calc").click(function (e) {
+    e.preventDefault();
+    caclProfit(e);
+  });
+
+  $("#btn_reset").click(function (e) {
+    e.preventDefault();
+    $("#form")[0].reset();
+    $("#profit").html("0");
+  });
+
+  $("body").on("keypress", (e) => {
+    if (e.key === "Enter") {
+      caclProfit(e);
+    }
+  });
+});
---
+++ skroutz-profit-calculator-v2/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html", "./skroutz-fees.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
