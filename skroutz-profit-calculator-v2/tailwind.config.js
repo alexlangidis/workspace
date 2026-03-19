a845425a46e168bc6f0fa1d2a2a82f152112216b
alexlangidis <alexlag.dev@gmail.com>
2026-03-19 23:10:14 +0200

    Move calculator breakpoint to 900px

--- skroutz-profit-calculator-v2/tailwind.config.js
+++ skroutz-profit-calculator-v2/tailwind.config.js
@@ -1,7 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+﻿/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html", "./skroutz-fees.html"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "900px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {},
   },
   plugins: [],
